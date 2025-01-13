1c40b028085b6348fce3822ff396979c86e8f4b8
Fekete János Dávid <janosdavidfekete@gmail.com>
2025-01-13 19:22:55 +0100

    datas not loading

--- src/app/base-service.service.ts
+++ src/app/base-service.service.ts
@@ -1,9 +1,46 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BaseServiceService {
+  private dataSubject = new Subject();
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.LoadAllMovies();
+   }
+
+  private url = 'http://localhost:3000/movies/';
+
+  LoadAllMovies() {
+    return this.http.get(this.url).subscribe(
+      (movies) => this.dataSubject.next(movies)
+    )
+  }
+
+  getAllMovies() {
+    return this.dataSubject;
+  }
+
+  getMovieByID(id: number) {
+    return this.http.get(this.url + id).forEach(() => this.LoadAllMovies());
+  }
+
+  createMovie(movie: any) {
+    return this.http.post(this.url, movie).forEach(() => this.LoadAllMovies());
+  }
+
+  updateMovie(id: number, movie: any) {
+    return this.http.put(this.url + id, movie).forEach(() => this.LoadAllMovies());
+  }
+
+  deleteMovie(id: number) {
+    return this.http.delete(this.url + id).forEach(() => this.LoadAllMovies());
+  }
+
+  patchMovie(id: number, movie: any) {
+    return this.http.patch(this.url + id, movie).forEach(() => this.LoadAllMovies());
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  movies:any
+
+  columnDefs = [
+    {key: 'id', text:'#', type: 'plain'},
+    {key:'title', text:'title', type:'text'},
+    {key:'director', text:'director', type:'text'},
+    {key:'genre', text:'genre', type:'text'},
+    {key:'release_year', text:'release year', type:'year'},
+    {key:'poster', text:'poster', type:'image'},
+  ]
+
+  newMovie:any = []
+  
+  
 
 }
