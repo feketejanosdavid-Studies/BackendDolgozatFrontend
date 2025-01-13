7684661e9e53082b669e7f14993543661fab88ac
Fekete János Dávid <janosdavidfekete@gmail.com>
2025-01-13 20:25:03 +0100

    Kész

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BaseServiceService } from '../base-service.service';
 
 @Component({
   selector: 'app-home',
@@ -14,11 +15,27 @@ export class HomeComponent {
     {key:'director', text:'director', type:'text'},
     {key:'genre', text:'genre', type:'text'},
     {key:'release_year', text:'release year', type:'year'},
-    {key:'poster', text:'poster', type:'image'},
+    {key:'poster', text:'poster', type:'url'}
   ]
 
   newMovie:any = []
+  constructor(private baseService: BaseServiceService) {
+    this.baseService.getAllMovies().subscribe(
+      movies => this.movies = movies
+    );
+  }
   
-  
+  addMovie() {
+    this.baseService.createMovie(this.newMovie)
+    this.newMovie = {}
+  }
+
+  editMovie(movie: any) {
+    this.baseService.updateMovie(movie.id, movie)
+  }
+
+  deleteMovie(id: number) {
+    this.baseService.deleteMovie(id)
+  }
 
 }
